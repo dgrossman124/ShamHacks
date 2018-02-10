e0c51acc5c4e87afd002eb8fad105bfa3e9fc463
Daniel Grossman <d.grossman@wustl.edu>
2018-02-10 17:45:20 -0600

    display table on main

--- main.js
+++ main.js
@@ -1,5 +1,16 @@
 var operations = require("operations");
 
-function addRow() {
-  
+function display(tableName) {
+  connection = connect();
+  connection.connect(function(err) {
+    if (err) throw err;
+    console.log("Connected!");
+    var insertQuery = "SELECT * FROM " + tableName;
+    connection.query(insertQuery, function (err, result) {
+      if (err) throw err;
+      console.log("Table displayed");
+    });
+  });
 }
+
+document.onload = display("audio");
