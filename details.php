731895126afe57e916e4af622da6e70401be85db
Daniel Grossman <d.grossman@wustl.edu>
2018-02-10 22:29:24 -0600

    details

---
+++ details.php
@@ -0,0 +1,29 @@
+<html>
+
+<head>
+</head>
+
+<body>
+
+  <div class="container" id="upload">
+    <article>
+      <?php
+        include('database.php');
+
+        $title = $_POST['title'];
+        $tableName = $_POST['table'];
+        echo '<header>'.title.'</header>';
+        $stmt = $mysqli->prepare("SELECT text FROM ".$tableName." WHERE title=".$title);
+        if(!$stmt){
+          printf("Query Prep Failed: %s\n", $mysqli->error);
+          exit;
+        }
+        $stmt->execute();
+        $stmt->bind_result($text);
+        echo $text;
+      ?>
+    </article>
+  </div>
+</body>
+
+</html>
