04f00984dfc7332747225b8f09bc8eb9f94f2436
Daniel Grossman <d.grossman@wustl.edu>
2018-02-10 18:13:03 -0600

    put query into table

--- main.js
+++ main.js
@@ -5,9 +5,14 @@ function display(tableName) {
   connection.connect(function(err) {
     if (err) throw err;
     console.log("Connected!");
-    var insertQuery = "SELECT * FROM " + tableName;
+    var insertQuery = "SELECT title, tags, participants, datetime FROM " + tableName;
     connection.query(insertQuery, function (err, result) {
       if (err) throw err;
+      var inner = "<tr><th>Title</th><th>Tags</th><th>Participants</th><th>Date Uploaded</th></tr>";
+      for(var i = 0; i < result.length; i++){
+        inner += "<tr><td>" + result[i].title + "</td><td>" + result[i].tags + "</td><td>" + result[i].participants + "</td><td>" + result[i].datetime + "</td></tr>";
+      }
+      document.getElementById("table").innerHTML = inner;
       console.log("Table displayed");
     });
   });
