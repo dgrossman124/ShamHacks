5469efdeec44a3e1ae8ec918602826fb9419c561
Daniel Grossman <d.grossman@wustl.edu>
2018-02-10 20:12:26 -0600

    added submit

--- server.js
+++ server.js
@@ -98,4 +98,12 @@ function display(tableName) {
   });
 }
 
-document.onload = display("audio");
+function submit(){
+  var radios = document.getElementsByName("fileType");
+  var isAudio = radios[0].checked;
+  var file = document.getElementById("myFile").name;
+  var title = document.getElementById("title").value;
+  operations.insertIntoDatabase(isAudio, file, title, title, title);
+}
+
+// document.onload = display("audio");
